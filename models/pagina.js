705ed3acc5f7ad076a8e7f1933062da7418f63b0
Diego Rangel Martínez <diegoranmtz@gmail.com>
2020-03-14 19:53:30 -0600

    domingo 14/03

--- models/pagina.js
+++ models/pagina.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var paginaSchema = new Schema({
   usuario: {type: String, required: [true, "La pagina es necesaria"]},
   paginaKey: {type: String, required: [true, "La key es necesaria"]},
-  accionKey: {type: String, unique: true, required: [true, "El nombre es necesaria"]},
+  accionKey: {type: String, required: [true, "El nombre es necesaria"]},
   precioBuy: {type: Number, required: [true, "El cuenta es necesaria"]},
   cantidad: {type: Number, required: [true, "La cantidad es necesaria"]}
 });
--- routes/pagina.js
+++ routes/pagina.js
@@ -174,4 +174,35 @@ app.delete("/:id", (req, res)=>{
     });
   });
 });
+//=========================================================
+// Select by accionKey and User only 4
+//=========================================================
+app.post("/accionUser/cuatro/",(req, res)=>{
+
+  var body = req.body;
+
+  Pagina.find({usuario: body.usuario},(err, paginaBD)=>{
+    if(err){
+      return res.status(500).json({
+        ok: false,
+        mensaje: "Error al buscar usuario",
+        errors: err
+      });
+    }
+
+    if(!paginaBD){
+      return res.status(200).json({
+        ok: false,
+        mensaje: "No hay registro",
+        errors: err
+      });
+    }
+
+    res.status(200).json({
+      ok: true,
+      acion: paginaBD,
+      id: paginaBD._id,
+    });
+  }).limit(4);
+});
 module.exports = app;
